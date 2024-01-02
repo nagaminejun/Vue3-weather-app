c30d2e11d4bbe32e96b10c085c905546ddd88fef
nagaminejun <nagaminejun1988@gmail.com>
2024-01-02 20:24:17 +0900

    Vue3の基礎学習　終了

---
+++ src/components/Results.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>気象データ</h2>
+  <div class v-if="results.country">{{ country }}</div>
+  <div class ="results-city" v-if="cityName">{{ cityName }}</div>
+  <div class ="results-temp" v-if="temperature">{{ temperature }}</div>
+  <div class ="results-condition" v-if="icon">
+    <img :src="results.icon" alt="icon">
+    <span>{{ results.conditionText }}</span>
+  </div>
+
+</template>
+
+<script setup>
+import { toRefs } from "vue"
+
+const props = defineProps({
+  results: Object
+})
+
+const { country, cityName, temperature, icon, conditionText } = toRefs(props.results)
+</script>
---
+++ src/components/Title.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="app-title">Vue World Weather</h1>
+</template>
+
+<style scoped>
+  .app-title{
+    color: red;
+  }
+</style>
